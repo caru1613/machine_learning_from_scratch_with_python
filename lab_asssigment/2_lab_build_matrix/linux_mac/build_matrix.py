a4c4cf106009c07535975e9fdd8116520fe3aaee
caru1613 <caru1613@gmail.com>
2022-03-20 09:11:10 +0900

    ch04 examples.
    Signed-off-by: caru1613 <caru1613@gmail.com>

--- lab_asssigment/2_lab_build_matrix/linux_mac/build_matrix.py
+++ lab_asssigment/2_lab_build_matrix/linux_mac/build_matrix.py
@@ -3,8 +3,27 @@ import pandas as pd
 
 
 def get_rating_matrix(filename, dtype=np.float32):
+    df_data = pd.read_csv(filename)
+    print(df_data)
+    print(df_data["source"])
+
+    # `    df_data.index = sorted(df_data["source"])
+    #    print(df_data)
+
+    #    del df_data["source"]
+    #    print(df_data)
+
     pass
 
 
 def get_frequent_matrix(filename, dtype=np.float32):
     pass
+
+
+def main():
+    get_rating_matrix("../movie_rating.csv")
+
+
+if __name__ == "__main__":
+    main()
+
